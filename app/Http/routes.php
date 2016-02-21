6eb50809c937f81d76f19bda828ffc042f24131b
Matt Floyd <mattdfloyd@gmail.com>
2016-02-21 13:04:49 -0500

    redirect base route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::any('/', function() {
+    return redirect('http://passioncitychurch.com/students');
+});
+
 Route::group(['middleware' => 'web'], function () {
     Route::get('register', 'RegisterController@create')->name('register.create');
     Route::post('register', 'RegisterController@store')->name('register.store');
