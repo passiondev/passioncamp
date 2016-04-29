e9624c9ff966d4f66d3c67d1445e71af2a92d6c9
Matt Floyd <mattdfloyd@gmail.com>
2016-04-29 12:12:25 -0400

    Add intro text and video to portal

--- resources/views/organization/show.blade.php
+++ resources/views/organization/show.blade.php
@@ -5,6 +5,11 @@
         <div class="Dashboard__header">
             <h1>{{ $organization->church->name }}</h1>
         </div>
+        <div class="callout primary" style="margin-bottom:2rem">
+            <h5>THE PASSION CAMP PORTAL IS HERE!</h5>
+            <p>We want to make all things Passion Camp as EASY as possible for you so this is going to be a valuable resource as you prepare for camp! The portal is the place for you to track your payments, provide us with your student and leader information, send waivers, and create your rooming list.</p>
+            <a data-open="portalIntroVideo" href="#">Check out the full portal tutorial!</a>
+        </div>
         <div class="row">
             <div class="small-6 columns">
                 <div class="Statistics_container">
@@ -61,3 +66,19 @@
         </div>
     </div>
 @stop
+
+@section('foot')
+<script>
+$(function(){
+    new Foundation.Reveal($('#portalIntroVideo'))
+})
+</script>
+<div class="reveal" id="portalIntroVideo" data-reveal data-reset-on-close="true">
+  <div class="flex-video widescreen">
+    <iframe src="https://player.vimeo.com/video/164488615" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+  </div>
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+@endsection
