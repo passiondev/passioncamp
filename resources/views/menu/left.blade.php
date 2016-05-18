0d79bc5aea2b538d16844f3046a0766b0dc436a5
Matt Floyd <mattdfloyd@gmail.com>
2016-05-18 13:16:57 -0400

    Conditionally hide roominglist tab

--- resources/views/menu/left.blade.php
+++ resources/views/menu/left.blade.php
@@ -10,7 +10,9 @@
 
 <a class="item {{ request()->route()->getName() == 'order.index' ? 'active' :'' }}" href="{{ route('order.index') }}">Registrations</a>
 <a class="item {{ request()->route()->getName() == 'ticket.index' ? 'active' :'' }}" href="{{ route('ticket.index') }}">Attendees</a>
-<a class="item {{ request()->route()->getName() == 'roominglist.index' ? 'active' :'' }}" href="{{ route('roominglist.index') }}">Rooming List</a>
+@if (auth()->user()->is_super_admin || auth()->user()->organization->rooms->count())
+    <a class="item {{ request()->route()->getName() == 'roominglist.index' ? 'active' :'' }}" href="{{ route('roominglist.index') }}">Rooming List</a>
+@endif
 
 @unless (auth()->user() && auth()->user()->is_super_admin)
     <a class="item {{ request()->route()->getName() == 'account.settings' ? 'active' :'' }}" href="{{ route('account.settings') }}">Account</a>
