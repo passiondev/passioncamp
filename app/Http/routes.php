a139b3bc0f07b1402f641d155e77b3edbe13427e
Matt Floyd <mattdfloyd@gmail.com>
2016-06-07 16:11:22 -0400

    Fix urls

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,9 +15,9 @@ Route::group(['middleware' => 'web'], function () {
         Route::any('/', function() {
             return redirect('http://passioncitychurch.com/students');
         });
-        Route::get('registration', 'RegisterController@create')->name('register.create');
-        Route::post('registration', 'RegisterController@store')->name('register.store');
-        Route::get('registration/confirmation', 'RegisterController@confirmation')->name('register.confirmation');
+        Route::get('register', 'RegisterController@create')->name('register.create');
+        Route::post('register', 'RegisterController@store')->name('register.store');
+        Route::get('register/confirmation', 'RegisterController@confirmation')->name('register.confirmation');
     });
 
     Route::any('echosign/callback', 'EchosignController@callback')->name('echosign.callback');
