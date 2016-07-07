904ef721078a017f2c2bdf3113d99f8a21cca4dc
Matt Floyd <mattdfloyd@gmail.com>
2016-07-07 10:14:06 -0400

    Update RL overview

--- resources/views/roominglist/overview.blade.php
+++ resources/views/roominglist/overview.blade.php
@@ -5,20 +5,31 @@
         <table class="ui striped table">
             <thead>
                 <tr>
-                    <th>Church</th>
-                    <th>Hotel</th>
-                    <th>Room</th>
-                    <th style="text-align:center">Capacity</th>
-                    <th style="text-align:center">Assigned</th>
-                    <th></th>
+                    <th class="four wide">Church</th>
+                    <th class="two wide">Hotel</th>
+                    <th class="two wide">Room</th>
+                    <th class="three wide">Description</th>
+                    <th class="three wide">Notes</th>
+                    <th class="" style="text-align:center">Capacity</th>
+                    <th class="" style="text-align:center">Assigned</th>
+                    <th class=""></th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($rooms as $room)
-                    <tr>
-                        <td>{{ $room->organization->church->name }} - {{ $room->organization->church->location }}</td>
+                    <tr class="{{ $room->capacity == $room->tickets->count() ? 'positive' : '' }} {{ $room->tickets->count() == 0 ? 'negative' : '' }}">
+                        <td>
+                            <h5 class="ui header">
+                                {{ link_to_route('admin.organization.show', $room->organization->church->name, $room->organization) }}
+                                <div class="sub header">
+                                    {{ $room->organization->church->location }}
+                                </div>
+                            </h5>
+                        </td>
                         <td>{{ $room->hotel->name }}</td>
                         <td>{{ $room->name }}</td>
+                        <td>{{ $room->description }}</td>
+                        <td><small>{{ $room->notes }}</small></td>
                         <td style="text-align:center">{{ $room->capacity }}</td>
                         <td style="text-align:center">{{ $room->tickets->count() }}</td>
                         <td><a href="{{ route('roominglist.edit', $room) }}">edit</a></td>
