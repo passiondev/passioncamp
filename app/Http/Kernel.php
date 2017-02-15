40b60e752ec0ad09ddb60eb211479d960961c249
Matt Floyd <mattdfloyd@gmail.com>
2017-02-15 14:58:05 -0500

    Upgrade Http Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,12 +47,14 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'super' => \App\Http\Middleware\VerifyUserIsSuperAdmin::class,
-        'church' => \App\Http\Middleware\VerifyUserIsChurchAdmin::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'super' => \App\Http\Middleware\VerifyUserIsSuperAdmin::class,
+        'church' => \App\Http\Middleware\VerifyUserIsChurchAdmin::class,
         'verify.auth.order' => \App\Http\Middleware\VerifyUserIsOrderAdmin::class,
         'admin' => \App\Http\Middleware\VerifyUserIsAdmin::class,
         'redirect.to.order' => \App\Http\Middleware\RedirectToSingleOrder::class,
