b1599eef9975c7c2dae8a6078740a24bb5d07fa4
Laravel Shift <shift@laravelshift.com>
2017-02-15 19:25:04 +0000

    Shift core files

--- app/User.php
+++ app/User.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use Notifiable;
     use SoftDeletes;
 
     protected $table = 'user';
