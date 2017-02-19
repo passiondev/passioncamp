8c4b87240c2dd728f833b0da3038fdff29b99fed
Matt Floyd <mattdfloyd@gmail.com>
2017-02-19 11:58:13 -0500

    Fix

--- resources/assets/js/components/RegisterFormTicket.vue
+++ resources/assets/js/components/RegisterFormTicket.vue
@@ -63,7 +63,8 @@
                         <option value="9">9th</option>
                         <option value="10">10th</option>
                         <option value="11">11th</option>
-                        <option value="12">12th</option></select>
+                        <option value="12">12th</option>
+                    </select>
                     <p class="form-text mb-0" style="line-height:1"><small class="text-muted">Grade completed as of Spring 2017.</small></p>
                 </div>
                 <div class="form-group col-lg-8">
