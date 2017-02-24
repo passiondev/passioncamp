ddf8acbf2cac0f6ada787450415946528b74fad6
Matt Floyd <mattdfloyd@gmail.com>
2017-02-24 10:39:07 -0500

    Add ability to alias an attribute

--- app/Model.php
+++ app/Model.php
@@ -6,4 +6,24 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
 {
+    public function getAttribute($key)
+    {
+        if ($this->hasAlias($key)) {
+            $this->attributes[$key] = $this->getAliasAttribute($key);
+
+            return $this->getAttributeValue($key);
+        }
+
+        return parent::getAttribute($key);
+    }
+
+    public function hasAlias($key)
+    {
+        return array_key_exists($key, $this->alias);
+    }
+
+    public function getAliasAttribute($key)
+    {
+        return $this->getAttribute($this->alias[$key]);
+    }
 }
