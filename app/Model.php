7b88a483aec4b57f145c588f04bd6a81af71cca1
Matt Floyd <mattdfloyd@gmail.com>
2017-02-24 10:38:47 -0500

    Remove asDateTime override

--- app/Model.php
+++ app/Model.php
@@ -6,8 +6,4 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
 {
-    protected function asDateTime($value)
-    {
-        return parent::asDateTime($value)->timezone('America/New_York');
-    }
 }
