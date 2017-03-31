d6966930044cd70bbd8f751db36079d3aca4450c
Matt Floyd <mattdfloyd@gmail.com>
2017-03-31 10:44:07 -0400

    Fix dashboard numbers

--- app/Http/Controllers/Super/DashboardController.php
+++ app/Http/Controllers/Super/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Super;
 
+use App\Organization;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
@@ -14,6 +15,17 @@ class DashboardController extends Controller
 
     public function __invoke()
     {
-        return view('admin.dashboard');
+        $data = [
+            'num_churches' => Organization::count(),
+            'num_tickets' => Organization::with('tickets')->get()->sumTicketQuantity(),
+            'total_cost' => Organization::totalCost() / 100,
+            'total_paid' => Organization::totalPaid() / 100,
+            'stripe' => Organization::totalPaid('stripe') / 100,
+            'other' => Organization::totalPaid('other') / 100,
+        ];
+
+        $data['balance'] = $data['total_cost'] - $data['total_paid'];
+
+        return view('admin.dashboard', compact('data'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,13 +5,13 @@
         <div class="card-deck mb-3">
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h1>{{ number_format(App\Organization::count()) }}</h1>
+                    <h1>{{ number_format($data['num_churches']) }}</h1>
                 </div>
                 <div class="card-footer text-muted">Churches</div>
             </div>
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h1>{{ number_format(App\Organization::with('tickets')->get()->sumTicketQuantity()) }}</h1>
+                    <h1>{{ number_format($data['num_tickets']) }}</h1>
                 </div>
                 <div class="card-footer text-muted">Tickets</div>
             </div>
@@ -19,13 +19,13 @@
         <div class="card-deck mb-3">
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h2>{{ money_format("%.0n", $totalCost = App\Organization::totalCost()) }}</h2>
+                    <h2>{{ money_format("%.0n", $data['total_cost']) }}</h2>
                 </div>
                 <div class="card-footer text-muted">Total Due</div>
             </div>
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h2>{{ money_format("%.0n", $totalCost - ($totalPaid = App\Organization::totalPaid())) }}</h2>
+                    <h2>{{ money_format("%.0n", $data['balance']) }}</h2>
                 </div>
                 <div class="card-footer text-muted">Balance Remaining</div>
             </div>
@@ -33,19 +33,19 @@
         <div class="card-deck mb-3">
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h2>{{ money_format("%.0n", $totalPaid) }}</h2>
+                    <h2>{{ money_format("%.0n", $data['total_paid']) }}</h2>
                 </div>
                 <div class="card-footer text-muted">Total Paid</div>
             </div>
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h3>{{ money_format("%.0n", App\Organization::totalPaid('stripe')) }}</h3>
+                    <h3>{{ money_format("%.0n", $data['stripe']) }}</h3>
                 </div>
                 <div class="card-footer text-muted">Stripe</div>
             </div>
             <div class="card mb-3 text-center">
                 <div class="card-block">
-                    <h3>{{ money_format("%.0n", App\Organization::totalPaid('other')) }}</h3>
+                    <h3>{{ money_format("%.0n", $data['other']) }}</h3>
                 </div>
                 <div class="card-footer text-muted">Check / Other</div>
             </div>
