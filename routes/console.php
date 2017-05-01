8c30112029c343805a6924e8672b5d81c12cba6f
Matt Floyd <mattdfloyd@gmail.com>
2017-05-01 18:50:35 -0400

    command to send balance due emails

--- routes/console.php
+++ routes/console.php
@@ -28,3 +28,22 @@ Artisan::command('passioncamp:deploy-rooms {organizationIds?*}', function ($orga
         }
     );
 });
+
+Artisan::command('pcc:balance-due', function () {
+    $orders = App\Order::where('organization_id', 1)
+        ->has('activeTickets')
+        ->get()
+        ->filter(function ($order) {
+            return $order->balance > 0;
+        });
+
+    $userIds = $orders->pluck('user_id')->unique();
+dd($userIds);
+    $sent = $userIds->each(function ($userId) {
+        dispatch(new App\Jobs\SendBalanceDueEmail(App\User::find($userId)));
+    });
+
+    $this->info('Orders: ' . $orders->count());
+    $this->info('Users: ' . $userIds->count());
+    $this->info('Sent: ' . $sent->count());
+});
