ca1378b483063c4ea74ce2d15286ac654d50a20c
Matt Floyd <mattdfloyd@gmail.com>
2017-06-01 13:50:24 -0400

    Close pcc registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -46,6 +46,8 @@ class RegisterController extends Controller
 
     public function create()
     {
+        return view('register.closed');
+
         return view('register.create')->withTicketPrice($this->ticket_price);
     }
 
---
+++ resources/views/register/closed.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.pccstudents')
+
+@section('content')
+    <div class="container mb-5">
+                <p class="lead">Camp Registration Is Now Full</p>
+
+                <p>Our team is working hard to make more spots available for camp. Please contact us at <a href="mailto:students@passioncitychurch.com">students@passioncitychurch.com</a> to be put on a waiting list. Thank you!</p>
+    </div>
+@stop
