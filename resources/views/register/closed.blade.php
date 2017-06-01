9a8daf76bb011414f5b2ae8990259aae74b79af0
Matt Floyd <mattdfloyd@gmail.com>
2017-06-01 13:57:45 -0400

    Update text

--- resources/views/register/closed.blade.php
+++ resources/views/register/closed.blade.php
@@ -4,6 +4,6 @@
     <div class="container mb-5">
                 <p class="lead">Camp Registration Is Now Full</p>
 
-                <p>Our team is working hard to make more spots available for camp. Please contact us at <a href="mailto:students@passioncitychurch.com">students@passioncitychurch.com</a> to be put on a waiting list. Thank you!</p>
+                <p>Our team is working hard to make additional spots available for camp. Please contact our team at <a href="mailto:students@passioncitychurch.com">students@passioncitychurch.com</a> or 404.231.7080 to be put on a waiting list. Thank you!</p>
     </div>
 @stop
