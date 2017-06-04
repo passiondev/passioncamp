a9924062240806cf80db2af2a0eab57741f89cc0
Matt Floyd <matt.floyd@268generation.com>
2017-06-04 19:59:07 -0400

    revert

--- app/Http/Controllers/TicketWristbandsController.php
+++ app/Http/Controllers/TicketWristbandsController.php
@@ -10,6 +10,7 @@ class TicketWristbandsController extends Controller
 {
     public function __construct()
     {
+        $this->middleware('auth');
         $this->middleware(VerifyPayloadSignature::class);
     }
 
