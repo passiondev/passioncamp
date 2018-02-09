a4790445a2465018552dba17c57fcefded1966a5
Matt Floyd <mattdfloyd@gmail.com>
2018-02-09 16:37:36 -0500

    Cleanup

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -20,6 +20,8 @@ class CheckinController extends Controller
 
     public function index()
     {
+        $tickets = [];
+
         if (request('search')) {
             $keys = Ticket::search(request('search'))
                 ->where('organization_id', auth()->user()->organization_id)
@@ -31,8 +33,6 @@ class CheckinController extends Controller
                 ])
                 ->with('person', 'order.user.items', 'order.user.transactions', 'waiver')
                 ->get();
-        } else {
-            $tickets = [];
         }
 
         $organization = auth()->user()->organization()->withCount('students', 'leaders', 'checkedInStudents', 'checkedInLeaders', 'activeAttendees')->first();
@@ -77,27 +77,4 @@ class CheckinController extends Controller
 
         return redirect()->action('CheckinController@index');
     }
-
-    // public function doCheckin(Request $request, Ticket $ticket)
-    // {
-    //     $ticket->checkin();
-
-    //     // generate pdf and send to printer
-    //     $pdf = new \HTML2PDF('L', [254, 25.4], 'en', true, 'UTF-8', [2*25.4,2*2.5,2.5,2.5]);
-    //     $pdf->writeHTML(view('ticket.wristband', compact('ticket'))->render());
-    //     $pdf->writeHTML(view('ticket.wristband', compact('ticket'))->render());
-
-    //     $handler = new PrintJobHandler(CheckinPrintNodeClient::init());
-    //     $handler->withPrinter($request->session()->get('printer'))->setTitle($ticket->id)->output($pdf);
-
-    //     return redirect()->route('checkin.index')->withTicketId($ticket->id)->withTicketName($ticket->person->name);
-    // }
-
-    // public function undoCheckin(Request $request, Ticket $ticket)
-    // {
-    //     $ticket->is_checked_in = false;
-    //     $ticket->save();
-
-    //     return redirect()->route('checkin.index')->withUncheck(true)->withTicketName($ticket->person->name);
-    // }
 }
