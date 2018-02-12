f2a0710822b02e97a9fcfbcd0049ad33d4fe8d9e
Matt Floyd <mattdfloyd@gmail.com>
2018-02-12 11:35:09 -0500

    Cleanup

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,14 +13,6 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        \App\Events\OrderItems\OrgItemUpdated::class => [
-            \App\Listeners\CreateRooms::class,
-        ],
-        \App\Events\Waivers\EmailBounced::class => [
-            \App\Listeners\Waivers\EmailBounced\CancelSignatureRequest::class,
-            \App\Listeners\Waivers\EmailBounced\CancelWaiverRecord::class,
-            \App\Listeners\Waivers\EmailBounced\SendNotification::class,
-        ],
         \App\Events\OrgItemSaved::class => [
             \App\Listeners\DispatchDeployRoomsForOrganization::class,
         ],
