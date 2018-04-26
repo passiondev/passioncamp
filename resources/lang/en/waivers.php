e927afba9dfe0c92441f5ceb8dc591325af23a9d
Matt Floyd <mattdfloyd@gmail.com>
2018-04-26 10:55:32 -0400

    Update waiver copy

--- resources/lang/en/waivers.php
+++ resources/lang/en/waivers.php
@@ -2,5 +2,5 @@
 
 return [
     'request_subject' => 'Passion Camp 2018 Waiver & Release',
-    'request_body' => 'Please note that each participant must have a signed waiver completed by May 29.',
+    'request_body' => 'Please note that each participant must have a signed waiver completed by June 4 to participate in Passion Camp.',
 ];
