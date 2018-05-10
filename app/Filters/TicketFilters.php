1c206f362d0ef41a169207701c961cd4f7b9894c
Matt Floyd <mattdfloyd@gmail.com>
2018-05-10 10:41:10 -0400

    Fix church search on tickets

--- app/Filters/TicketFilters.php
+++ app/Filters/TicketFilters.php
@@ -8,6 +8,8 @@ class TicketFilters extends Filters
 
     protected function organization($organization_id)
     {
-        return $this->builder->select('order_items.*')->join('orders', 'orders.id', '=', 'order_id')->where('orders.organization_id', $organization_id);
+        return $this->builder->select('order_items.*')->join('orders', function ($q) {
+            $q->whereRaw('owner_id = orders.id AND owner_type = "App\\\Order"');
+        })->where('orders.organization_id', $organization_id);
     }
 }
