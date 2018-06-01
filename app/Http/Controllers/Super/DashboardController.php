f0cdd96fe8b2ce665246fda474807320bff254ab
Matt Floyd <mattdfloyd@gmail.com>
2018-06-01 09:34:42 -0400

    Add to dashboard stats for api

--- app/Http/Controllers/Super/DashboardController.php
+++ app/Http/Controllers/Super/DashboardController.php
@@ -9,6 +9,8 @@ class DashboardController extends Controller
 {
     public function __invoke()
     {
+        $organizations = Organization::with('settings')->get();
+
         $data = [
             'num_churches' => Organization::active()->count(),
             'num_tickets' => Organization::withTicketsSum()->get()->sum('tickets_sum'),
@@ -16,6 +18,14 @@ class DashboardController extends Controller
             'total_paid' => Organization::withPaidSum()->get()->sum('paid_sum') / 100,
             'stripe' => Organization::withPaidSum('stripe')->get()->sum('stripe_paid_sum') / 100,
             'other' => Organization::withPaidSum('other')->get()->sum('other_paid_sum') / 100,
+            'balance' => $organizations->sum('cached_balance') / 100,
+            'tickets_sum' => $organizations->sum('cached_tickets_sum'),
+            'active_attendees_count' => $organizations->sum('cached_active_attendees_count'),
+            'assigned_to_room_count' => $organizations->sum('cached_assigned_to_room_count'),
+            'completed_waivers_count' => $organizations->sum('cached_completed_waivers_count'),
+            'rooms_count' => $organizations->sum('cached_rooms_count'),
+            'key_received_rooms_count' => $organizations->sum('cached_key_received_rooms_count'),
+            'is_checked_in' => $organizations->where('is_checked_in', true)->count(),
         ];
 
         $data['balance'] = $data['total_cost'] - $data['total_paid'];
