9f8c8fe1808033d8adcd99c1d5773bcf3440cab3
Matt Floyd <mattdfloyd@gmail.com>
2018-09-14 17:28:01 -0400

    Setup import

--- app/Console/Commands/ImportPayments.php
+++ app/Console/Commands/ImportPayments.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
-use League\Csv\Reader;
 use App\Organization;
+use League\Csv\Reader;
+use Illuminate\Console\Command;
 
 class ImportPayments extends Command
 {
@@ -22,7 +22,7 @@ class ImportPayments extends Command
      */
     public function handle()
     {
-        $csv = Reader::createFromPath(storage_path('app/import2018-payments.csv'));
+        $csv = Reader::createFromPath(storage_path('app/2019/payments.csv'));
         $csv->setHeaderOffset(0);
 
         collect($csv->getRecords())
@@ -32,7 +32,7 @@ class ImportPayments extends Command
                 $organization->addTransaction([
                     'source' => $row['source'],
                     'identifier' => $row['identifier'],
-                    'amount' => $row['amount'] * 100,
+                    'amount' => (int) $row['amount'] * 100,
                 ]);
             });
     }
---
+++ database/seeds/TestDataSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+use App\Item;
+use App\User;
+use App\Hotel;
+use App\Order;
+use App\Ticket;
+use App\OrgItem;
+use App\Organization;
+use App\TransactionSplit;
+use Illuminate\Database\Seeder;
+
+class TestDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $org = factory(Organization::class)->create();
+
+        factory(User::class)->states(['churchAdmin'])->create([
+            'email' => 'church@example.com',
+            'organization_id' => $org,
+        ]);
+
+        $org->tickets()->save(
+            factory(OrgItem::class)->make([
+                'item_id' => factory(Item::class)->create([
+                    'type' => 'ticket',
+                    'name' => 'Tcket',
+                ]),
+                'org_type' => 'ticket',
+                'cost' => 39900,
+                'quantity' => 30,
+            ])
+        );
+
+        $org->hotelItems()->save(
+            factory(OrgItem::class)->make([
+                'item_id' => factory(Hotel::class)->create(),
+                'org_type' => 'hotel',
+                'cost' => 0,
+                'quantity' => 6,
+            ])
+        );
+
+        factory(TransactionSplit::class, 2)->create([
+            'organization_id' => $org,
+        ]);
+
+        $org->orders()->save(
+            $order = factory(Order::class)->make([
+                'organization_id' => null,
+            ])
+        );
+
+        $order->tickets()->saveMany(
+            factory(Ticket::class, 3)->make()
+        );
+    }
+}
