92665dff4741aeed7045329508c798441d4c4250
Matt Floyd <mattdfloyd@gmail.com>
2019-02-21 13:15:41 -0500

    wip

--- config/occurrences.php
+++ config/occurrences.php
@@ -13,7 +13,7 @@ return [
             '2018-12-02' => '115',
             '2018-12-31' => '130',
         ],
-        'closes' => '2019-01-27 23:59:59',
+        'closes' => '2019-07-27 23:59:59',
         'closed_message' => 'Thank you so much for your interest in Middle School Winter WKND. At this time registration has closed. Please email [students@passioncitychurch.com](mailto:students@passioncitychurch.com) for further details or if you have any questions. Thanks so much!',
         'confirmation' => 'We are so excited your student is planning to join us for Middle School Winter WKND! We are looking forward to all that Jesus is going to do in the lives of our students during our time together!',
         'donation_list' => "- A Family Group Leader\'s spot (We will have 40-50 leaders with us.)\n- Ground Transportation (Each van costs $420 and we will have 10-12 vans!)\n- To help a student who can’t afford Winter WKND (We don’t want anyone to miss out!)",
--- routes/pccstudents.php
+++ routes/pccstudents.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::any('/', function () {
-    return redirect('http://passioncitychurch.com/students');
-});
+Route::redirect('/', 'http://passioncitychurch.com/students');
 Route::get('register', 'RegisterController@create')->name('register.create');
 Route::post('register', 'RegisterController@store')->name('register.store');
 Route::get('register/confirmation', 'RegisterController@confirmation')->name('register.confirmation');
