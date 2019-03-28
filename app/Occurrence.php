e8abe6df7f3440e5388d43a609f0631354dc45c4
Matt Floyd <mattdfloyd@gmail.com>
2019-03-27 21:37:13 -0400

    wip

--- app/Occurrence.php
+++ app/Occurrence.php
@@ -26,9 +26,163 @@ class Occurrence
         // multi ticket auto discount removed 3/6/19
 
         switch (strtolower($discountCode)) {
-            // case 'discount':
-            //     $price -= 10;
-            //     break;
+            // generic
+            case 'staff2019':
+                $price = $price * .8;
+                break;
+            case 'pc2019':
+                $price = $price - 20;
+                break;
+            case 'trickshot':
+                $price = $price - 15;
+                break;
+
+            case 'goodfriday':
+                if (now()->between(Carbon::parse('2019-04-19 00:00:00'), Carbon::parse('2019-04-19 23:59:59'))) {
+                    $price = $price - 15;
+                }
+                break;
+            case 'easter':
+                if (now()->between(Carbon::parse('2019-04-21 00:00:00'), Carbon::parse('2019-04-21 23:59:59'))) {
+                    $price = $price - 25;
+                }
+                break;
+            case 'sadie':
+                if (now()->between(Carbon::parse('2019-04-24 09:00:00'), Carbon::parse('2019-04-26 23:59:59'))) {
+                    $price = $price - 15;
+                }
+                break;
+            case 'rising':
+                if (now()->between(Carbon::parse('2019-04-24 05:00:00'), Carbon::parse('2019-04-26 23:59:59'))) {
+                    $price = $price - 15;
+                }
+                break;
+            case 'tuesday':
+                if (now()->between(Carbon::parse('2019-05-14 00:00:00'), Carbon::parse('2019-05-14 23:59:59'))) {
+                    $price = $price - 15;
+                }
+                break;
+            case 'seniors':
+                if (now()->between(Carbon::parse('2019-05-19 00:00:00'), Carbon::parse('2019-05-19 23:59:59'))) {
+                    $price = $price - 15;
+                }
+                break;
+            case 'summer':
+                if (now()->between(Carbon::parse('2019-05-24 00:00:00'), Carbon::parse('2019-05-27 23:59:59'))) {
+                    $price = $price - 15;
+                }
+                break;
+
+            // scholarships
+            case 'scholarship50':
+                $price = $price - 50;
+                break;
+            case 'scholarship100':
+                $price = $price - 100;
+                break;
+            case 'scholarship150':
+                $price = $price - 150;
+                break;
+            case 'scholarship185':
+                $price = $price - 185;
+                break;
+            case 'scholarship200':
+                $price = $price - 200;
+                break;
+
+            // schools
+            case 'ahs19':
+            case 'cambridge19':
+            case 'dhs19':
+            case 'fcs19':
+            case 'gac19':
+            case 'grady19':
+            case 'kingsridge19':
+            case 'lakeside19':
+            case 'landmark19':
+            case 'mtpisgah19':
+            case 'pope19':
+            case 'wesleyan19':
+            case 'whitefield19':
+                $price = $price - 10;
+                break;
+
+            // student rep
+            case 'studentrep':
+            case 'rosap':
+            case 'gregf':
+            case 'lillianh':
+            case 'robbier':
+            case 'abbyw':
+            case 'taylorw':
+            case 'abbeyv':
+            case 'anniek':
+            case 'brendanp':
+            case 'carleel':
+            case 'gabbyg':
+            case 'kimberlyg':
+            case 'brooklynnp':
+            case 'alexl':
+            case 'daphner':
+            case 'ellaw':
+            case 'caseyt':
+            case 'joew':
+            case 'isabeld':
+            case 'marcelaa':
+            case 'haileyp':
+            case 'andresj':
+            case 'kylaw':
+            case 'jadac':
+            case 'dyllonj':
+            case 'carlyw':
+            case 'sawyert':
+            case 'evanw':
+            case 'kennedyp':
+            case 'bellam':
+            case 'nanm':
+            case 'hannahw':
+            case 'julias':
+            case 'samh':
+            case 'haleym':
+            case 'emmack':
+            case 'brianj':
+            case 'megand':
+            case 'kierand':
+            case 'tylerb':
+            case 'patrickm':
+            case 'jaanas':
+            case 'susannahh':
+            case 'madelynw':
+            case 'haleya':
+            case 'gracec':
+            case 'alliem':
+            case 'austins':
+            case 'mitchelln':
+            case 'richardy':
+            case 'emmas':
+            case 'averym':
+            case 'isabellah':
+            case 'harrisond':
+            case 'elleno':
+            case 'jacobb':
+            case 'carlya':
+            case 'carlolinek':
+            case 'isaiaho':
+            case 'margaretd':
+            case 'allier':
+            case 'madiganw':
+            case 'hannahs':
+            case 'evelyns':
+            case 'annab':
+            case 'lizs':
+            case 'emilyd':
+            case 'paulinem':
+            case 'gibs':
+            case 'danielt':
+            case 'ashleye':
+            case 'willh':
+                $price = $price - 10;
+                break;
         }
 
         return $price * 100;
