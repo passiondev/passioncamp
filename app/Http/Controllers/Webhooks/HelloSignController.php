a3638c1829174c19f2e83899e1b73e128fe2dadb
Matt Floyd <mattdfloyd@gmail.com>
2019-05-06 21:45:47 -0400

    fix

--- app/Http/Controllers/Webhooks/HelloSignController.php
+++ app/Http/Controllers/Webhooks/HelloSignController.php
@@ -26,7 +26,7 @@ class HelloSignController extends Controller
             return response('not found', 404);
         }
 
-        logger()->info($request->input('event.event_type'), ['signature_request_id' => $request->input('event.event_metadata.related_signature_id')]);
+        logger()->info($request->input('event.event_type'), ['signature_request_id' => $request->input('signature_request.signature_request_id')]);
 
         switch ($request->input('event.event_type')) {
             case 'signature_request_signed':
@@ -35,14 +35,14 @@ class HelloSignController extends Controller
             case 'signature_request_downloadable':
             case 'signature_request_all_signed':
                 $waiver = Waiver::whereProvider('hellosign')
-                    ->where('provider_agreement_id', $request->input('event.event_metadata.related_signature_id'))
+                    ->where('provider_agreement_id', $request->input('signature_request.signature_request_id'))
                     ->firstOrFail();
                 FetchAndUpdateStatus::dispatch($waiver);
                 break;
 
             case 'signature_request_email_bounce':
                 $waiver = Waiver::whereProvider('hellosign')
-                    ->where('provider_agreement_id', $request->input('event.event_metadata.related_signature_id'))
+                    ->where('provider_agreement_id', $request->input('signature_request.signature_request_id'))
                     ->firstOrFail();
                 $waiver->update(['status' => 'bounced']);
                 break;
