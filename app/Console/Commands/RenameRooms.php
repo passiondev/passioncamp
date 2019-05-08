39e64e9e8ec819edc9cea854f85fdad8842e33c0
Matt Floyd <mattdfloyd@gmail.com>
2019-05-08 15:55:44 -0400

    allow passing org id to rename room command

--- app/Console/Commands/RenameRooms.php
+++ app/Console/Commands/RenameRooms.php
@@ -7,19 +7,25 @@ use App\Organization;
 
 class RenameRooms extends Command
 {
-    protected $signature = 'passioncamp:rename-rooms';
+    protected $signature = 'passioncamp:rename-rooms {organizationIds?*}';
 
     public function handle()
     {
-        Organization::has('rooms')->each(function ($organization) {
-            $this->comment('org: ' . $organization->id);
-            $i = 1;
-            $organization->rooms()->orderBy('id')->each(function ($room) use (&$i) {
-                $this->comment('room: ' . $room->id);
-                $room->update([
-                    'name' => 'Room #' . $i++
-                ]);
+        $orgIds = $this->argument('organizationIds');
+
+        Organization::has('rooms')
+            ->when(! empty($orgIds), function ($q) use ($orgIds) {
+                $q->whereIn('id', $orgIds);
+            })
+            ->each(function ($organization) {
+                $this->comment('org: ' . $organization->id);
+                $i = 1;
+                $organization->rooms()->orderBy('id')->each(function ($room) use (&$i) {
+                    $this->comment('room: ' . $room->id);
+                    $room->update([
+                        'name' => 'Room #' . $i++
+                    ]);
+                });
             });
-        });
     }
 }
