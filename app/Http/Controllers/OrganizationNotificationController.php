421d22b7c9a70138bceff8e819e5122a3c5efec7
Matt Floyd <mattdfloyd@gmail.com>
2020-02-08 15:40:55 -0500

    wip

--- app/Http/Controllers/OrganizationNotificationController.php
+++ app/Http/Controllers/OrganizationNotificationController.php
@@ -5,12 +5,25 @@ namespace App\Http\Controllers;
 use App\Notifications\OrganizationNotification;
 use App\Organization;
 use Illuminate\Http\Request;
+use Illuminate\Notifications\DatabaseNotification;
 
 class OrganizationNotificationController extends Controller
 {
     public function __construct()
     {
         $this->middleware(['auth', 'can:super']);
+    }
+
+    public function index()
+    {
+        $notifications = DatabaseNotification::query()
+            ->where('type', OrganizationNotification::class)
+            ->latest()
+            ->paginate(5);
+
+        $organizations = Organization::active()->orderByChurchName()->get();
+
+        return view('organization-notifications.index', compact('notifications', 'organizations'));
     }
 
     public function store(Request $request)
@@ -26,5 +39,14 @@ class OrganizationNotificationController extends Controller
         $organizations->each->notify(
             new OrganizationNotification($request->input('subject'))
         );
+
+        return redirect()->back()->with('success', 'Notification sent.');
+    }
+
+    public function destroy(DatabaseNotification $notification)
+    {
+        $notification->delete();
+
+        return redirect()->back()->with('success', 'Notification deleted.');
     }
 }
---
+++ resources/views/organization-notifications/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.bootstrap4')
+
+@section('content')
+    <div class="container-fluid">
+        <header class="d-lg-flex justify-content-between align-items-center mb-lg-2">
+            <h1 class="mb-2 mb-lg-0">Notifications</h1>
+        </header>
+
+        <div class="card mt-4">
+            <h3 class="card-header">New Notification</h3>
+            <div class="card-block">
+                <form action="{{ action('OrganizationNotificationController@store') }}" method="POST" onsubmit="return confirm('This will send a notification to the selected church(es). Are you sure?')">
+                    @csrf
+
+                    <div class="form-group">
+                        <label for="organization">Church</label>
+                        <select name="organization" class="form-control mb-2 mr-sm-2 mb-sm-0">
+                            <option selected value="">- All -</option>
+                            @foreach ($organizations as $organization)
+                                <option value="{{ $organization->id }}" @if (request('organization') == $organization->id) selected @endif>
+                                    {{ $organization->church->name }} - {{ $organization->church->location }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="subject">Message</label>
+                        <input type="text" class="form-control" name="subject" id="subject" required>
+                    </div>
+
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="card mt-4 mb-2" id="sent">
+            <h3 class="card-header">Sent Notifications</h3>
+            <div>
+                <table class="table table-responsive table-striped mb-0">
+                    @foreach ($notifications as $notification)
+                        <tr>
+                            <td>
+                                {{ $notification->notifiable->church->name }}
+                            </td>
+                            <td>
+                                <small>{{ Str::limit($notification->data['subject'], 20) }}</small>
+                            </td>
+                            <td>
+                                {{ $notification->read() ? 'read' : 'new' }}
+                            </td>
+                            <td>
+                                {{ $notification->created_at }}
+                            </td>
+                            <td>
+                                <form action="{{ action('OrganizationNotificationController@destroy', $notification) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this notification?')">
+                                    @method('delete')
+                                    @csrf
+                                    <button type="submit" class="btn btn-sm btn-link">delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+        {{ $notifications->fragment('sent')->links() }}
+    </div>
+@stop
