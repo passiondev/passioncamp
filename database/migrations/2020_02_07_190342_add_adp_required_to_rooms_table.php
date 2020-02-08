1498ab3907bb500190368baddfd940d6cd523bf3
Matt Floyd <mattdfloyd@gmail.com>
2020-02-07 19:12:44 -0500

    wip

--- database/migrations/2020_02_07_190342_add_adp_required_to_rooms_table.php
+++ database/migrations/2020_02_07_190342_add_adp_required_to_rooms_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddAdpRequiredToRoomsTable extends Migration
 {
@@ -14,7 +14,7 @@ class AddAdpRequiredToRoomsTable extends Migration
     public function up()
     {
         Schema::table('rooms', function (Blueprint $table) {
-            //
+            $table->boolean('adp_required')->nullable();
         });
     }
 
--- database/migrations/2020_02_07_190446_add_kin_preferred_to_rooms_table.php
+++ database/migrations/2020_02_07_190446_add_kin_preferred_to_rooms_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddKinPreferredToRoomsTable extends Migration
 {
@@ -14,7 +14,7 @@ class AddKinPreferredToRoomsTable extends Migration
     public function up()
     {
         Schema::table('rooms', function (Blueprint $table) {
-            //
+            $table->boolean('king_preferred')->nullable();
         });
     }
 
