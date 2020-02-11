fbfa18dc72a0fe1a4b29e0b3fcba45b8c70572f9
Matt Floyd <mattdfloyd@gmail.com>
2020-02-11 15:48:46 -0500

    wip

---
+++ database/migrations/2020_02_11_154735_add_indexes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            $table->index('canceled_at');
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->index('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_items', function (Blueprint $table) {
+            //
+        });
+    }
+}
