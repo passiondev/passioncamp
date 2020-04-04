dc0cadba430250eca4f29067b1265cae8f9ea6a4
Matt Floyd <mattdfloyd@gmail.com>
2020-04-04 14:07:58 -0400

    Remove price increases

--- config/occurrences.php
+++ config/occurrences.php
@@ -9,8 +9,8 @@ return [
         'header_fg' => 'https://res.cloudinary.com/passionconf/image/upload/f_auto,q_auto/v1583760654/passioncamp2020/pcc/fg.png',
         'pricing' => [
             '2020-01-01' => '399',
-            '2020-04-06' => '425',
-            '2020-05-04' => '450',
+            // '2020-04-06' => '425',
+            // '2020-05-04' => '450',
         ],
         'closes' => '2020-06-07 23:59:59',
         'closed_message' => 'Thank you so much for your interest in Passion Camp. At this time registration has closed. Please email [students@passioncitychurch.com](mailto:students@passioncitychurch.com) for further details or if you have any questions. Thanks so much!',
