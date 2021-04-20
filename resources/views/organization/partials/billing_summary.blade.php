3f8013904119724aaad8a559c0845ad3dd09c738
Jon Woodson <jon.woodson@passioncitychurch.com>
2021-04-20 17:01:52 -0400

    Update billing_summary.blade.php

--- resources/views/organization/partials/billing_summary.blade.php
+++ resources/views/organization/partials/billing_summary.blade.php
@@ -70,7 +70,7 @@
                         @endcan
                     </div>
                     <div class="item right item--{{ $split->amount>0 ? 'success' : 'warning' }}">
-                        {{ number_format($split->amount / 100, 2, '.', '') }}
+                        {{ $split->amount }}
                     </div>
                 </div>
                 @if ($split->transaction->source == 'stripe')
